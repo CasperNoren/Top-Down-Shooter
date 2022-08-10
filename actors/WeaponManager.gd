aa9d7e27cb22adb3c4ef576afb228b5e272b20c4
Casper Norén <casper.noren01@gmail.com>
2022-08-10 02:21:28 +0200

    Made weapon selection scrollable

--- actors/WeaponManager.gd
+++ actors/WeaponManager.gd
@@ -5,6 +5,7 @@ signal weapon_changed(new_weapon)
 onready var current_weapon: Weapon = $Pistol
 
 var weapons: Array = []
+var current_weapon_index = 0
 
 func _ready():
 	weapons = get_children()
@@ -34,7 +35,14 @@ func switch_weapon(weapon: Weapon):
 	current_weapon.hide()
 	weapon.show()
 	current_weapon = weapon
+	current_weapon_index = weapons.find(current_weapon,0)
 	emit_signal("weapon_changed", current_weapon)
+
+func get_weapon_from_index(index: int) -> Weapon:
+	if index < 0:
+		index = weapons.size() - 1
+	index = index % weapons.size()
+	return weapons[index]
 
 func _unhandled_input(event):
 	if current_weapon.semi_auto and event.is_action_released("shoot"):
@@ -42,6 +50,8 @@ func _unhandled_input(event):
 	elif event.is_action_pressed("reload"):
 		reload()
 	elif event.is_action_pressed("change_weapon_up"):
-		switch_weapon(weapons[0])
+		var next_weapon: Weapon = get_weapon_from_index(current_weapon_index + 1)
+		switch_weapon(next_weapon)
 	elif event.is_action_pressed("change_weapon_down"):
-		switch_weapon(weapons[1])
+		var next_weapon: Weapon = get_weapon_from_index(current_weapon_index - 1)
+		switch_weapon(next_weapon)
