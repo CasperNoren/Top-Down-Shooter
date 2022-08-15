1d800be0f8535d897bbed7a345f7530db165cd48
Casper Norén <casper.noren01@gmail.com>
2022-08-15 15:56:25 +0200

    Added test buy function

--- MoneyManager.gd
+++ MoneyManager.gd
@@ -14,9 +14,15 @@ func handle_bases_changed(bases):
 		if base.team.team == team.team:
 			number_of_team_bases += 1
 
-
 func _on_Timer_timeout():
 	money += number_of_team_bases
 	print(str(team.team) + " money: " + str(money) + "\nNumber of bases: " + str(number_of_team_bases))
 
+func _unhandled_input(event):
+	if team.team == Team.TeamName.PLAYER and event.is_action_released("buy"):
+		try_buy()
 
+func try_buy():
+	if money >= 20:
+		money -= 20
+		print("Bought")
