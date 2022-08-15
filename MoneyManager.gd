3a2900824c75d9af0a2832a93ea6ca36eb526ba9
Casper Norén <casper.noren01@gmail.com>
2022-08-15 02:02:30 +0200

    Made Money Manager general
    Added a team to Money Manager

--- MoneyManager.gd
+++ MoneyManager.gd
@@ -1,16 +1,17 @@
 extends Node2D
 
+onready var team = $Team
 
-var player_money: int = 0;
-var number_of_player_bases: int = 0
+var money: int = 0;
+var number_of_team_bases: int = 0
 
 func handle_bases_changed(bases):
-	number_of_player_bases = 0
+	number_of_team_bases = 0
 	for base in bases:
-		if base.team.team == Team.TeamName.PLAYER:
-			number_of_player_bases += 1
+		if base.team.team == team.team:
+			number_of_team_bases += 1
 
 
 func _on_Timer_timeout():
-	player_money += number_of_player_bases
-	print("Player money: " + str(player_money) + "\nNumber of player bases: " + str(number_of_player_bases))
+	money += number_of_team_bases
+	print(str(team.team) + " money: " + str(money) + "\nNumber of bases: " + str(number_of_team_bases))
