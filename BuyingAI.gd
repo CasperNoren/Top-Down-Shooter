79af0cf2d10224b2b1ca8cc69fc84088688626a8
Casper Norén <casper.noren01@gmail.com>
2022-08-19 23:03:38 +0200

    Buying AI helper functions

--- BuyingAI.gd
+++ BuyingAI.gd
@@ -1,11 +1,28 @@
 extends Node2D
 
+signal buying_ai_choose_option(option)
+
 # Should have priorities like: 
 # 	if the player has more actors -> prioritize more actors
 # 	if only the home base is left -> prioritize turrets
 # 	else -> buy weapons
 # Maybe priority should be an int that can be changed depending on factors?
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var available_options: Array = []
+var money: int = 0
+
+func choose_next_purchase():
+	pass
+
+# TODO: Make moneymanager call this when removed_options has been updated
+# Returns all options that aren't removed
+func set_available_options(all_options: Array, removed_options: Array) -> Array:
+	available_options = []
+	for option in all_options:
+		# -1 means that the option is not in the list
+		if removed_options.find(option) == -1:
+			available_options.append(option)
+	return available_options
+
+func set_money(money: int):
+	self.money = money 
