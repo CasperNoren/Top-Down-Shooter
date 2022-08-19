7119fce83540217f88c85b5ecc50fae81be5555c
Casper Norén <casper.noren01@gmail.com>
2022-08-19 18:05:54 +0200

    BuyingAI node added and planning comments

---
+++ BuyingAI.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+# Should have priorities like: 
+# 	if the player has more actors -> prioritize more actors
+# 	if only the home base is left -> prioritize turrets
+# 	else -> buy weapons
+# Maybe priority should be an int that can be changed depending on factors?
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
