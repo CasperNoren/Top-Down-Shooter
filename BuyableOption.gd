c1f14ab6b5f27985f6ca12363adbb99d909f1d03
Casper Norén <casper.noren01@gmail.com>
2022-08-23 21:28:09 +0200

    BuyableOption type

--- BuyableOption.gd
+++ BuyableOption.gd
@@ -2,8 +2,17 @@ extends Node2D
 
 class_name BuyableOption
 
+enum OptionType {
+	TEAM,
+	DEFENSE,
+	WEAPON
+}
+
 export (PackedScene) var item_scene = null
 export (String) var signal_to_emit = ""
 export (String) var shop_name = ""
 export (int) var cost = 0
 export (bool) var should_not_be_removed = false
+
+export (OptionType) var option_type = OptionType.WEAPON
+
